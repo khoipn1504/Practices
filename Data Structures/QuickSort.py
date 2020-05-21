8804198c42e83bf03210851bb643f1216378d458
khoipn1504 <neron0men@gmail.com>
2020-05-21 13:14:16 +0700

    FormatListToString

--- Data Structures/QuickSort.py
+++
@@ -1,2 +0,0 @@
-a=[0,3,5]*2
-print(str(a) +'asfasf')
---
+++ I should know soon/FormatListToString.py
@@ -0,0 +1,12 @@
+names=[{'name': 'Homer'},{'name': 'Homer'},{'name': 'Homer'},]
+
+def namelist(names):
+    if len(names) > 1:
+        return '{} & {}'.format(', '.join(name['name'] for name in names[:-1]),
+                                names[-1]['name'])
+    elif names:
+        return names[0]['name']
+    else:
+        return ''
+
+print(namelist(names))
