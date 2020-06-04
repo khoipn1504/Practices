0a4acb4167c5ec59e92526a8568d1138504572ee
khoipn1504 <neron0men@gmail.com>
2020-06-04 09:30:51 +0700

    last update p4

--- Pycharm_edu/Tic-Tac-Toe/p4.py
+++ Pycharm_edu/Tic-Tac-Toe/p4.py
@@ -1,4 +1,4 @@
-inp = list('X_X_O____')
+inp = list(input("Enter cells: ").upper().replace("_", " "))
 rows = [inp[0:3], inp[3:6], inp[6:9]]
 cols = [inp[0:7:3], inp[1:8:3], inp[2:9:3]]
 diags = [inp[0:9:4], inp[2:7:2]]
@@ -23,9 +23,9 @@ while True:
         print("Coordinates should be from 1 to 3!")
         continue
 
-    coor = (3-user[0], user[1]-1)
+    coor = (3-user[1], user[0]-1)  # cai nay quan trong
 
-    if rows[coor[0]][coor[1]] != "_" and rows[coor[0]][coor[1]] != " ":
+    if rows[coor[0]][coor[1]] != " ":
         print("This cell is occupied! Choose another one!")
         continue
 
