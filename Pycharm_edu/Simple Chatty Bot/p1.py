b640022a12fb14c5efc625c905dbf07ae17fd260
khoipn1504 <neron0men@gmail.com>
2020-06-05 10:26:23 +0700

    update

---
+++ Pycharm_edu/Simple Chatty Bot/p1.py
@@ -0,0 +1,2 @@
+print("Hello! My name is {bot_name}.".format(bot_name="wYw"))
+print("I was created in {birth_year}.".format(birth_year="2020"))
---
+++ Pycharm_edu/Simple Chatty Bot/p2.py
@@ -0,0 +1,7 @@
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+user_name = input()
+print(f'What a great name you have, {user_name}!')
